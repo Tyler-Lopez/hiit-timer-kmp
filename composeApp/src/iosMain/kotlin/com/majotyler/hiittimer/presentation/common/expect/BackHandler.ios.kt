04fda10d6439977156f6d702000a1f44bb825836
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2026-01-04 00:46:15 -0700

    Refactor Create Workout Screen
    I realized that the Create Workout Screen is actually really complicated and wanted to explore a refactor of just the UI in a way that I hope will make it easier for users to understand what they need to do.
    
    This commit introduces that refactor, only touching the ViewModel layer to demonstrate changing pages, which was a strategy to break up the complication of the page.

---
+++ composeApp/src/iosMain/kotlin/com/majotyler/hiittimer/presentation/common/expect/BackHandler.ios.kt
@@ -0,0 +1,7 @@
+package com.majotyler.hiittimer.presentation.common.expect
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {
+}
