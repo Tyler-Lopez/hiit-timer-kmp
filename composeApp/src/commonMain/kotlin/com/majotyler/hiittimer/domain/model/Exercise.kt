60eefaf99c4ef57700924a6338b1def464500f1f
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2026-01-04 19:02:15 -0700

    Create data models up to Interval
    This commit adds the main data models we will need.
    
    The most atomic unit is the `Exercise`, which is used by `Workout`, which is used by `Interval`.
    
    Finally, I'm not sure what it should be called, but a `List` of `Interval` is what we will actually be using to run the app.

---
+++ composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/domain/model/Exercise.kt
@@ -0,0 +1,7 @@
+package com.majotyler.hiittimer.domain.model
+
+data class Exercise(
+    val duration: Int,
+    val name: String,
+    val rest: Int,
+)
---
+++ composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/domain/model/Workout.kt
@@ -0,0 +1,6 @@
+package com.majotyler.hiittimer.domain.model
+
+data class Workout(
+    val exercises: List<Exercise>,
+    val name: String,
+)
