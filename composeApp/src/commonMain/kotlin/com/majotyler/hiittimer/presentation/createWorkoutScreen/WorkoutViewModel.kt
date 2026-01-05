dd89cada2f3569efe6f774670a260787634aefa1
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2026-01-04 17:10:20 -0700

    fixup! Remove pages in favor of single page ui

--- composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/createWorkoutScreen/WorkoutViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/createWorkoutScreen/WorkoutViewModel.kt
@@ -12,14 +12,10 @@ class WorkoutViewModel(private val router: Router<WorkoutDestination>) : ViewMod
     private val _enabled = MutableStateFlow(false)
     val enabled = _enabled.asStateFlow()
 
-    private val _page = MutableStateFlow(value = CreateWorkoutPage.entries.first())
-    val page = _page.asStateFlow()
-
     fun onEvent(event: WorkoutViewEvent) {
         when (event) {
             is WorkoutViewEvent.NameWorkout -> onNameWorkout(event.newNameWorkout)
             is WorkoutViewEvent.AddWorkout -> onAddWorkout()
-            is WorkoutViewEvent.ClickedAdvanceButton -> onClickedAdvanceButton()
             is WorkoutViewEvent.ClickedNavigateUp -> onClickedNavigateUp()
         }
     }
@@ -33,21 +29,8 @@ class WorkoutViewModel(private val router: Router<WorkoutDestination>) : ViewMod
         router.routeTo(destination = WorkoutDestination.NavigateToTimer)
     }
 
-    private fun onClickedAdvanceButton() {
-        val newOrdinal = _page.value.ordinal + 1
-
-        if (newOrdinal <= CreateWorkoutPage.entries.lastIndex) {
-            _page.value = CreateWorkoutPage.entries.get(index = newOrdinal)
-        } else {
-            // TODO, this indicates we should create and add the workout
-        }
-    }
-
     private fun onClickedNavigateUp() {
-        val newOrdinal = _page.value.ordinal - 1
-
-        if (newOrdinal >= 0) {
-            _page.value = CreateWorkoutPage.entries.get(index = newOrdinal)
-        }
+        // TODO (This needs to navigate up so you go back to the previous screen, maybe with a
+        // "are you sure" type message
     }
 }
