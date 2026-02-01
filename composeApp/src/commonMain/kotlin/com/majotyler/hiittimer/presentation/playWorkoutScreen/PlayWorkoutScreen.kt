ce30615fdb7739a281f61b951679934b58f91ec0
Maria1-eng <142539017+Maria1-eng@users.noreply.github.com>
2026-01-31 22:22:46 -0600

    Preview playWorkout  Screen
    This commit implements a preview of PlayWorkout Screen using fake values

--- composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/playWorkoutScreen/PlayWorkoutScreen.kt
+++ composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/playWorkoutScreen/PlayWorkoutScreen.kt
@@ -1,14 +1,29 @@
 package com.majotyler.hiittimer.presentation.playWorkoutScreen
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -16,38 +31,76 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun PlayWorkoutScreen(
     viewModel: PlayWorkoutVIewModel,
 ) {
-    val state = viewModel.state.collectAsStateWithLifecycle()
-
-    val seconds = state.value.seconds
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    val seconds = state.seconds
+    val play by viewModel.play.collectAsStateWithLifecycle()
+    val text by viewModel.text.collectAsStateWithLifecycle()
+    val enabled by viewModel.enabled.collectAsStateWithLifecycle()
 
     PlayWorkoutContent(
         seconds = seconds,
-        onEvent = { event -> viewModel.onEvent(event = event) },
+        onClickedPlay = { viewModel.onEvent(event = PlayWorkoutViewEvent.ClickedPlay) },
+        text = text,
+        play = play,
+        onClickedPause = {
+            viewModel.onEvent(
+                PlayWorkoutViewEvent.ClickedPause
+            )
+        }, enabled, onEnabled = { viewModel.onEvent(PlayWorkoutViewEvent.Enabled) }
     )
 }
 
 @Composable
 private fun PlayWorkoutContent(
     seconds: Int,
-    onEvent: (PlayWorkoutViewEvent) -> Unit,
+    onClickedPlay: () -> Unit,
+    text: String,
+    play: Boolean,
+    onClickedPause: () -> Unit,
+    enabled: Boolean,
+    onEnabled: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background)
-            .safeDrawingPadding(), contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Placeholder text.")
-    }
-}
+    val totalTime = 50
+    val progress by animateFloatAsState(seconds / totalTime.toFloat())
+    LaunchedEffect(progress) {
+        if (progress >= 1f) {
+            onClickedPause()
+            onEnabled()
 
-//region Previews
-@Preview
-@Composable
-private fun PlayWorkoutContent_Preview_Zero_Seconds() {
-    PlayWorkoutContent(
-        seconds = 0,
-        onEvent = {},
-    )
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            Modifier.size(200.dp).drawBehind {
+                drawArc(
+                    startAngle = 180F, sweepAngle = 360F, useCenter = false,
+                    style = Stroke(width = 10f), color = Color.LightGray
+                )
+
+                drawArc(
+                    startAngle = 180F,
+                    sweepAngle = 360f * progress,
+                    useCenter = false,
+                    style = Stroke(width = 12f),
+                    color = Color.Green
+                )
+            },
+            contentAlignment = Alignment.Center
+        ) {
+            Text("$seconds")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = {
+            onClickedPlay()
+        }, enabled = enabled) {
+            Text(text)
+        }
+
+
+    }
+
 }
-//endregion Previews
