04fe678b3d27215d03397646275eb9d9f22813e4
Maria1-eng <142539017+Maria1-eng@users.noreply.github.com>
2026-04-08 21:43:07 -0600

    Next Steps
    This implements timer progression to next steps

--- composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/playWorkoutScreen/PlayWorkoutVIewModel.kt
+++ composeApp/src/commonMain/kotlin/com/majotyler/hiittimer/presentation/playWorkoutScreen/PlayWorkoutVIewModel.kt
@@ -38,28 +38,71 @@ class PlayWorkoutVIewModel(
     val enabled = _enabled.asStateFlow()
 
     private var currentStepProgressMs: Long = 0L
-    private val currentStepTotalMs: Long
+    private val currentStepTotalMs: Long // Cada vez que utilizo currentStepTotalMs el codigo de abajo se ejecuta, todo el valor de abajo se guardara momentaneamente en currentStepTotalMs
         get() = currentWorkoutInterval
-            ?.run {
-                val seconds = when (currentWorkoutIntervalState) {
+            ?.run{
+                val seconds = when (currentWorkoutIntervalState) { // Asigna a Seconds la duracion o el descanso
                     PlayWorkoutStateOfInterval.EXERCISING -> duration
                     PlayWorkoutStateOfInterval.RESTING -> rest
                 }
                 val milliseconds = seconds * MILLISECONDS_IN_SECOND
                 milliseconds
             } ?: 0L
+    // get() = currentWorkoutInterval
+    //            ?.run{ this -> // puedes hacer this.duration o duration porque this por que es un apuntador ya esta en currentWotkoutInterval es que es de tipo Interval
+    //              Interval es @Serializable  data class Interval(
+    //    val duration: Int,
+    //    val name: String,
+    //    val rest: Int,
+    //)
+    //                val seconds = when (currentWorkoutIntervalState) {
+    //                    PlayWorkoutStateOfInterval.EXERCISING -> this.duration
+    //                    PlayWorkoutStateOfInterval.RESTING -> this.rest
+    //                }
+    //                val milliseconds = seconds * MILLISECONDS_IN_SECOND
+    //                milliseconds
+    //            } ?: 0L
+//    fun currentStepTotal(): Long {
+////        val currentWorkoutIntervalInmutable = currentWorkoutInterval
+//        if (currentWorkoutInterval!= null){
+//            currentWorkoutIntervalIndex = -50
+//            val seconds = when (currentWorkoutIntervalState) {
+//                PlayWorkoutStateOfInterval.EXERCISING -> currentWorkoutInterval.duration
+//                PlayWorkoutStateOfInterval.RESTING -> currentWorkoutInterval.rest
+//            }
+//            val milliseconds = seconds * MILLISECONDS_IN_SECOND
+//            return milliseconds
+//        }else{
+//            return 0L
+//        }
+//    }
 
     /** The index of the current [Workout] in [workouts]. */
     private var currentWorkoutIndex: Int = 0
     private val currentWorkout: Workout?
-        get() = workouts.getOrNull(index = currentWorkoutIndex)
+        get() = workouts.getOrNull(index = currentWorkoutIndex) // obtiene el valor de un workout es de tipo Workout entonces tiene que verse como eso
 
     /** The index of the current [Interval] of [currentWorkout] */
     private var currentWorkoutIntervalIndex: Int = 0
-    private var currentWorkoutIntervalState = PlayWorkoutStateOfInterval.EXERCISING
-    private val currentWorkoutInterval: Interval?
+    private var currentWorkoutIntervalState = PlayWorkoutStateOfInterval.EXERCISING // si hicieramos un print aqui diria 'EXERCISING' porque tiene un nombre interno
+    private val currentWorkoutInterval: Interval? // de tipo interval
         get() = currentWorkout?.intervals?.getOrNull(index = currentWorkoutIntervalIndex)
+        // si no es null currentWorkout  -> si no es null intervals -> entra a intervals que es una lista de tipo interval que es una data class
+        // se veria algo como
+        // data class Workout(
+        //    val intervals: List<Interval>,
+        //    val name: String,
+        //    val repetitions: Int, )
 
+        //      intervals = listOf(
+        //        Interval(30, "Jumping Jacks", 10),
+        //        Interval(40, "Push Ups", 15),
+        //        Interval(60, "Squats", 20)
+        //    )
+        //    y con el indice obten un interval
+//    fun CurrentWorkoutInterval(): Interval?{
+//        return currentWorkout?.intervals?.getOrNull(index = currentWorkoutIntervalIndex)
+//    }
     private var pollProgressJob: Job? = null
     private var runningMark: TimeMark? = null
 
@@ -157,14 +200,39 @@ class PlayWorkoutVIewModel(
 
                     val hasReachedEndOfCurrentStep: Boolean = newState.progress >= 1F
                     if (hasReachedEndOfCurrentStep) {
-                        _play.value = false
-                        _text.value = "Start"
-                        _enabled.value = false
-                        pollProgressJob?.cancel()
+                        val lastIndexOfIntervalsInCurrentWorkout = currentWorkout?.intervals?.lastIndex
+                        val isResting = currentWorkoutIntervalState == PlayWorkoutStateOfInterval.RESTING
+                        val currentIntervalIsLastInWorkout = currentWorkoutIntervalIndex == lastIndexOfIntervalsInCurrentWorkout
+                        val hasFinishedAllWorkouts = isResting && currentIntervalIsLastInWorkout
+
+
+                       if (hasFinishedAllWorkouts){
+                           _play.value = false
+                           _text.value = "Start"
+                           _enabled.value = false
+                           pollProgressJob?.cancel()
+                       }else {
+                           nextStep()
+                       }
+
                     }
                 }
             }
         }
+    }
+
+    private fun nextStep(){
+        when (currentWorkoutIntervalState){
+            PlayWorkoutStateOfInterval.EXERCISING -> { currentWorkoutIntervalState =
+                PlayWorkoutStateOfInterval.RESTING
+            }
+            PlayWorkoutStateOfInterval.RESTING -> {
+                currentWorkoutIntervalIndex++
+                currentWorkoutIntervalState =
+                    PlayWorkoutStateOfInterval.EXERCISING
+            }
+        }
+        currentStepProgressMs = 0L
     }
 
     companion object {
